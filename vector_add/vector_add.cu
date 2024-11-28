abdfe1530bf4cb746f215f316f6d2c642c45bb3e
sreiswig <sam.reiswig@gmail.com>
2024-11-27 19:08:33 -0600

    debugging why vector add doesn't work

--- vector_add/vector_add.cu
+++ vector_add/vector_add.cu
@@ -15,6 +15,31 @@ __global__ void vector_add(float *out, float *a, float *b, int n) {
 }
 
 int main(){
+  
+  int nDevices;
+  cudaGetDeviceCount(&nDevices);
+  
+  printf("Number of devices: %d\n", nDevices);
+  
+  for (int i = 0; i < nDevices; i++) {
+      cudaDeviceProp prop;
+      cudaGetDeviceProperties(&prop, i);
+      printf("Device Number: %d\n", i);
+      printf("  Device name: %s\n", prop.name);
+      printf("  Memory Clock Rate (MHz): %d\n",
+           prop.memoryClockRate/1024);
+      printf("  Memory Bus Width (bits): %d\n",
+           prop.memoryBusWidth);
+      printf("  Peak Memory Bandwidth (GB/s): %.1f\n",
+           2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+      printf("  Total global memory (Gbytes) %.1f\n",(float)(prop.totalGlobalMem)/1024.0/1024.0/1024.0);
+      printf("  Shared memory per block (Kbytes) %.1f\n",(float)(prop.sharedMemPerBlock)/1024.0);
+      printf("  minor-major: %d-%d\n", prop.minor, prop.major);
+      printf("  Warp-size: %d\n", prop.warpSize);
+      printf("  Concurrent kernels: %s\n", prop.concurrentKernels ? "yes" : "no");
+      printf("  Concurrent computation/communication: %s\n\n",prop.deviceOverlap ? "yes" : "no");
+    }
+
     float *a, *b, *out;
     float *d_a, *d_b, *d_out; 
 
@@ -38,14 +63,20 @@ int main(){
     cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, b, sizeof(float) * N, cudaMemcpyHostToDevice);
 
+    printf("a[0] = %f\n", a[0]);
+    printf("b[0] = %f\n", b[0]);
+
     // Executing kernel 
     vector_add<<<1,1>>>(d_out, d_a, d_b, N);
-    
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+      printf("CUDA Error: %s\n", cudaGetErrorString(err));
+    }
+
     // Transfer data back to host memory
     cudaMemcpy(out, d_out, sizeof(float) * N, cudaMemcpyDeviceToHost);
 
     printf("out[0] = %f\n", out[0]);
-    printf("PASSED\n");
 
     // Deallocate device memory
     cudaFree(d_a);
